b5bacb2fc0e9bbae589f76ff677189bce00cfdaf
dominickestle <dominickestle23@gmail.com>
2022-02-22 15:14:37 +0000

    did runtime todos 1 - 3 part one

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -36,11 +36,17 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.rect(canvasWidth,groundY + 2,'lightblue');
             background.addChild(backgroundFill);
             
             // TODO: 3 - Add a moon and starfield
             
+            var moon = draw.bitmap('img/moon.png'); //created a variable called moon. draw.bitmap draws the image and stores it in the moon variable
+            moon.x = 1500; //determines the x position of the moon
+            moon.y = 100; //determines the y position of the moon
+            moon.scaleX = 0.5; //determines the length of the moon
+            moon.scaleY = 0.5; //determines the height of the moon
+            background.addChild(moon); //adds the moon to the canvas so we can see it 
             
             // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
             
