68fd461524077c7ff197828c8b30b47483cb4aea
dominickestle <dominickestle23@gmail.com>
2022-03-16 14:14:26 +0000

    finished all ToDos

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -16,9 +16,17 @@ var level01 = function (window) {
             "number": 1, 
             "speed": -3,
             "gameItems": [
-                { "type": "sawblade", "x": 400, "y": groundY },
-                { "type": "sawblade", "x": 600, "y": groundY },
-                { "type": "sawblade", "x": 900, "y": groundY },
+                { "type": "sawblade", "x": 400, "y": 360 },
+                { "type": "sawblade", "x": 600, "y": 360 },
+                { "type": "sawblade", "x": 800, "y": 360 },
+
+                { "type": "enemy", "x": 450, "y": groundY - 50 },
+                { "type": "enemy", "x": 650, "y": groundY - 50 },
+                { "type": "enemy", "x": 850, "y": groundY - 50 },
+
+                { "type": "reward", "x": 1050, "y": groundY - 50 },
+                { "type": "reward", "x": 1250, "y": groundY - 50 },
+                { "type": "reward", "x": 1450, "y": groundY - 50 },
             ]
         };
         window.levelData = levelData;
@@ -42,10 +50,6 @@ var level01 = function (window) {
             obstacleImage.y = -25; //tweaks the image 25 pixels up
             sawBladeHitZone.rotationalVelocity = 10;
         }
-
-        createSawBlade(400, 360);
-        createSawBlade(600, 360);
-        createSawBlade(800, 360);
 
         function createEnemy(x, y){
             var enemy = game.createGameItem('enemy',25); //creating the game item and storing it in the variable enemy
@@ -76,9 +80,45 @@ var level01 = function (window) {
             };
         }
 
-        createEnemy(450, groundY - 50);
-        createEnemy(650, groundY - 50);
-        createEnemy(850, groundY - 50);
+        for (var i = 0; i < levelData.gameItems.length; i++){
+            var gameItem = levelData.gameItems[i];
+            
+            if (gameItem.type === "sawblade"){
+                createSawBlade(gameItem.x, gameItem.y);
+            } 
+
+            if (gameItem.type === "enemy"){
+                createEnemy(gameItem.x, gameItem.y);
+            }
+
+            if (gameItem.type === "reward"){
+                createReward(gameItem.x, gameItem.y);
+            }
+        }
+        
+        function createReward(x, y){
+            var reward = game.createGameItem('reward',25); //creating the game item and storing it in the variable reward
+            var blueSquare = draw.rect(50,50,'blue'); //creates a rectangle and stores as blueSquare
+            blueSquare.x = -25; 
+            blueSquare.y = -25;
+            reward.addChild(blueSquare); //adds the blu square to the enemy game item
+
+            reward.x = x;
+            reward.y = y;
+
+            game.addGameItem(reward); //adds reward to the game
+
+            reward.velocityX = -1; //this causes the rward to move one pixel to the left on the x position
+
+            reward.rotationalVelocity = 25; //this make the reward rotate
+
+            reward.onPlayerCollision = function() {
+                console.log('The reward has hit Halle');
+                game.changeIntegrity(25);
+                game.increaseScore(10);
+                reward.shrink();
+            };
+        }
 
         // DO NOT EDIT CODE BELOW HERE
     }
